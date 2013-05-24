04348dbff6469e57d819f0c72727f5c97e5ce60e
HyunjeJo <retriever89@gmail.com>
2013-05-24 11:05:02 +0900

    BasicHandler 추가

--- src/main/java/org/herring/agent/sender/handler/BasicHandler.java
+++ src/main/java/org/herring/agent/sender/handler/BasicHandler.java
@@ -12,22 +12,26 @@ import org.herring.protocol.handler.MessageHandler;
 public final class BasicHandler extends MessageHandler {
     @Override
     public void messageArrived(NetworkContext context, Object data) throws Exception {
-
+        System.out.println("Arrived Data : "+ data);
     }
 
     @Override
     public void channelReady(NetworkContext context) throws Exception {
+        System.out.println("Connection Ready");
     }
 
     @Override
     public void channelInactive(NetworkContext context) throws Exception {
+        System.out.println("Connection Inactivated");
     }
 
     @Override
     public void channelClosed(NetworkContext context) throws Exception {
+        System.out.println("Connection Finished");
     }
 
     @Override
     public void networkStopped() throws Exception {
+        System.out.println("NetWork Stopped");
     }
 }
