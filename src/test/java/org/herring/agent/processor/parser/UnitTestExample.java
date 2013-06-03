4244f9814cdbe128d2a274f69f334e95afc6a033
HyunjeJo <retriever89@gmail.com>
2013-06-04 02:38:15 +0900

    Test 연습 Class 이름 변경

--- src/test/java/org/herring/agent/processor/parser/UnitTestExample.java
+++ src/test/java/org/herring/agent/processor/parser/UnitTestExample.java
@@ -11,7 +11,7 @@ import org.junit.runners.MethodSorters;
  */
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class AbstractParserTest {
+public class UnitTestExample {
 
     @Test
     public void parserTest()throws Exception{
